8caed622a2bd3f10b0d0f0dad98c259c85734ca6
CA Solutions <33277778+casolutions2@users.noreply.github.com>
2021-11-11 06:14:36 +1000

    Update MongoDBTest.cpp

--- MongoDBTest.cpp
+++ MongoDBTest.cpp
@@ -10,6 +10,14 @@
 #pragma resource "*.dfm"
 TForm1 *Form1;
 //---------------------------------------------------------------------------
+static const String _Server 	= "128.199.141.172";
+static const int 	_Port 	= 27017;
+static const String _Db      = "MTR_CMS";
+static const String _auth    = "authSource=MTR_CMS";
+static const String _uid     = "MTRCMS";
+static const String _pwd     = "4ca72fd9a32b226c46b6247f15d57daf";
+
+//---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
@@ -28,3 +36,39 @@ void __fastcall TForm1::Button1Click(TObject *Sender)
 	}
 	delete doc;
 }
+
+void __fastcall TdmMongo::InitiateDB() {
+	#ifdef __MACOS__
+		return;
+	#else
+		// windows
+		con->SpecificOptions->Values["MongoDB.BSONLibrary"] = "C:\\Program Files (x86)\\Devart\\UniDAC for RAD Studio 10.3\\Bin\\Win32\\libbson-1.0.dll";
+		con->SpecificOptions->Values["MongoDB.ClientLibrary"] = "C:\\Program Files (x86)\\Devart\\UniDAC for RAD Studio 10.3\\Bin\\Win32\\libmongoc-1.0.dll";
+		con->SpecificOptions->Values["ConnectionOptions"] = _auth;
+
+	#endif
+	con->Server 	= _Server;
+	con->Port   	= _Port;
+	con->Database   = _Db;
+	con->Username   = _uid;
+	con->Password   = _pwd;
+
+	// tblUsers options
+	tblUsers->TableName     = "Users";
+	tblUsers->SpecificOptions->Values["ComplexAsString"] 	= "False";
+	tblUsers->SpecificOptions->Values["AllowAddField"]      = "True";
+}
+
+//---------------------------------------------------------------------------
+bool __fastcall TdmMongo::ConnectToDB() {
+	try {
+		InitiateDB();
+		con->Open();
+	} catch ( const Exception& ex ) {
+		ShowMessage(ex.Message);
+    }
+
+	return con->Connected;
+}
+//---------------------------------------------------------------------------
+
